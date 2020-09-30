fb96306d6b95444f26d16db45b1376ca8e425926
SF <smfoit4311@gmail.com>
2020-09-29 20:48:59 -0500

    Added Sword FX

---
+++ Assets/Scripts/SwordFXPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordFXPlayer : MonoBehaviour
+{
+    public ParticleSystem[] swordFX;
+
+    public void PlaySlashFX(int play)
+    {
+        swordFX[play].Play();
+    }
+}
